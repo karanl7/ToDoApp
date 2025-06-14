72b04a887c4597ba282e55aa6526431409ae67b2
karan <karannisar811@gmail.com>
2025-06-14 18:13:15 +0530

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
+export default function TodoList(){
+    let [todos, setTodos] = useState([{task: "sample Task", id: uuidv4(), isDone:false}]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewtask = () => {
+        setTodos((prevTodos) => {
+            return [...prevTodos, {task: newTodo, id: uuidv4(), isDone:false}];
+        });
+           
+        setNewTodo("");
+    };
+
+    let updateTodoValue =(event) => {
+        setNewTodo(event.target.value);
+    };
+
+    let deleteTodo = (id) => {
+        setTodos((prevTodos) =>todos.filter((prevTodos) => prevTodos.id != id));
+    };
+
+    let markAsDone = (id) =>{
+        setTodos((prevTodos)=> {
+            return prevTodos.map((todo)=>{
+                if(todo.id == id){
+                    return{
+                        ...todo,
+                        isDone: true,
+                    };
+                }else{
+                    return todo;
+                }
+                
+
+            });
+        });
+    };
+
+    return(
+        <div>
+            <input placeholder = "add a task" value={newTodo}
+            onChange={updateTodoValue}></input>
+            <br></br>
+            <br></br>
+            <button onClick={addNewtask}>Add a Task</button>
+            <br></br>
+            <br></br>
+
+            <hr></hr>
+            <h4>Tasks Todo</h4>
+            <ul>
+                {todos.map((todo)=>(
+                     <li key = {todo.id}>
+                        <span style={todo.isDone ? {textDecorationLine: "line-through"}: {}}>{todo.task}</span>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={()=>deleteTodo(todo.id)}>delete</button> <span></span> 
+                        <button onClick={()=>markAsDone(todo.id)}>Mark As Done</button>
+                        </li>
+
+                    )
+                )
+                }
+            </ul>
+        </div>
+    );
+}
