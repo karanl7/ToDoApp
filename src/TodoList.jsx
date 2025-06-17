540721841a4ff73420ad3c1843e2116851dd2907
VanshiGala <galavanshi0207@gmail.com>
2025-06-17 18:11:02 +0530

    Changed the backgroundColor

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,69 +1,73 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
+export default function TodoList() {
+  let [todos, setTodos] = useState([
+    { task: "sample Task", id: uuidv4(), isDone: false },
+  ]);
+  let [newTodo, setNewTodo] = useState("");
 
-export default function TodoList(){
-    let [todos, setTodos] = useState([{task: "sample Task", id: uuidv4(), isDone:false}]);
-    let [newTodo, setNewTodo] = useState("");
+  let addNewtask = () => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4(), isDone: false }];
+    });
 
-    let addNewtask = () => {
-        setTodos((prevTodos) => {
-            return [...prevTodos, {task: newTodo, id: uuidv4(), isDone:false}];
-        });
-           
-        setNewTodo("");
-    };
+    setNewTodo("");
+  };
 
-    let updateTodoValue =(event) => {
-        setNewTodo(event.target.value);
-    };
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
 
-    let deleteTodo = (id) => {
-        setTodos((prevTodos) =>todos.filter((prevTodos) => prevTodos.id != id));
-    };
+  let deleteTodo = (id) => {
+    setTodos((prevTodos) => todos.filter((prevTodos) => prevTodos.id != id));
+  };
 
-    let markAsDone = (id) =>{
-        setTodos((prevTodos)=> {
-            return prevTodos.map((todo)=>{
-                if(todo.id == id){
-                    return{
-                        ...todo,
-                        isDone: true,
-                    };
-                }else{
-                    return todo;
-                }
-                
+  let markAsDone = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      });
+    });
+  };
 
-            });
-        });
-    };
+  return (
+    <div className="bg-blue-100">
+      <input
+        placeholder="add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      ></input>
+      <br></br>
+      <br></br>
+      <button onClick={addNewtask}>Add a Task</button>
+      <br></br>
+      <br></br>
 
-    return(
-        <div>
-            <input placeholder = "add a task" value={newTodo}
-            onChange={updateTodoValue}></input>
-            <br></br>
-            <br></br>
-            <button onClick={addNewtask}>Add a Task</button>
-            <br></br>
-            <br></br>
-
-            <hr></hr>
-            <h4>Tasks Todo</h4>
-            <ul>
-                {todos.map((todo)=>(
-                     <li key = {todo.id}>
-                        <span style={todo.isDone ? {textDecorationLine: "line-through"}: {}}>{todo.task}</span>
-                        &nbsp;&nbsp;&nbsp;
-                        <button onClick={()=>deleteTodo(todo.id)}>delete</button> <span></span> 
-                        <button onClick={()=>markAsDone(todo.id)}>Mark As Done</button>
-                        </li>
-
-                    )
-                )
-                }
-            </ul>
-        </div>
-    );
+      <hr></hr>
+      <h4>Tasks Todo</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={todo.isDone ? { textDecorationLine: "line-through" } : {}}
+            >
+              {todo.task}
+            </span>
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>{" "}
+            <span></span>
+            <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
